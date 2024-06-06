eca8ec02804bfe0983905e1e2dbb3a95b979f86e
vozKingdom <161555246+vozKingdom@users.noreply.github.com>
2024-06-06 20:09:17 +0700

    Breathe calculator to life w JS (barely)
    - Breathe calculator to life with questionable
    JS code. It is on life support though. Needs more
    work done, which we will begin tmrw.
    
    Need to:
    - Finish JS code to bring calculator to full
    operation
    - Clean (refactor) JS code to guarantee SUCCESS
    - have a gn 😘

--- script.js
+++ script.js
@@ -1,9 +1,113 @@
-console.log('~ final hours. make it count. ~')
+console.log('~ final hours. make it count. ~') 
 
 
- let num1;
- let operator;
- let num2;
+
+//select display screen
+const screen = document.querySelector('.calculator-screen-text')
+
+
+//select all btns. then convert nodelist to array
+const btnsNodeList = document.querySelectorAll('button')
+const btns = Array.from(btnsNodeList);
+
+
+//setup btn 'click' event listener 
+
+btns.forEach((btn)=>btn.addEventListener('click', (event)=>{
+
+    console.log(operator)
+
+    let input = btn.textContent
+
+        if(input == '='){
+            let ans = operate(numA, operator, numB)
+            return screen.textContent = ans
+        }
+
+
+
+        if((!OPERATORS.includes(input)) &&
+           (!operator)
+        ){
+            console.log('numA clicked')
+
+            if (!numA){
+                numA = input
+            } else {
+                numA += input
+            }
+            
+            screen.textContent = numA
+        }
+
+
+        if(OPERATORS.includes(input)){
+
+            console.log('OPERATOR clicked')
+
+            operator = event.target.textContent
+            console.log(Boolean(operator))
+
+            screen.textContent = operator
+        }
+
+        if((!OPERATORS.includes(input)) &&
+           (operator))
+            {
+            console.log('numB clicked')
+
+            if (!numB){
+                numB = input
+            } else {
+                numB += input
+            }
+            
+            screen.textContent = numB
+        }
+
+
+
+
+
+
+
+        console.log(`${numA} - this is numA`)
+        console.log(`(${numA},${operator}) - this is (numA, operator)`)
+        console.log(`(${numA},${operator},${numB}) - this is (numA, operator, numB)`)
+        // else {
+        //     screen.textContent = input
+        //     num1 = input
+        // }
+
+
+        if((input === 'C') ||
+           (input === 'Del')
+          ){
+            console.log('ORANGE MAN clicked')
+
+            numA = undefined
+            numB = undefined
+            operator = undefined
+
+            return screen.textContent = 0
+        } 
+    })
+)
+
+
+
+
+let numA;
+let numB;
+let operator;
+
+
+const OPERATORS = '+-*/%'
+
+
+
+
+
 
 
 
@@ -16,7 +120,7 @@ function operate(num1, operator, num2){
 
 
 function add(num1, num2){
-    total = num1 + num2    
+    total = +num1 + +num2    
     return total 
 }
 
