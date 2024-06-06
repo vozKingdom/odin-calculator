0f41dffcd409170d02a3692a9a451bb4a703fa15
vozKingdom <161555246+vozKingdom@users.noreply.github.com>
2024-06-06 12:09:23 +0700

    Add basic js code for calculator operations

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 console.log('~ final hours. make it count. ~')
+
+
+ let num1;
+ let operator;
+ let num2;
+
+
+
+function operate(num1, operator, num2){
+    if (operator == '+') return add(num1, num2) 
+    if (operator == '-') return subtract(num1, num2) 
+    if (operator == '*') return multiply(num1, num2) 
+    if (operator == '/') return divide(num1, num2) 
+}
+
+
+function add(num1, num2){
+    total = num1 + num2    
+    return total 
+}
+
+
+function subtract(num1, num2){
+    total = num1 - num2
+    return total 
+}
+
+function multiply(num1, num2){
+    total = num1 * num2
+    return total 
+}
+
+
+function divide(num1, num2){
+    total = num1/num2
+    return total 
+}
+
