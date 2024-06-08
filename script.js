886f504c7a53c2103277fe5c8e63119c9cf74df9
vozKingdom <161555246+vozKingdom@users.noreply.github.com>
2024-06-08 16:55:25 +0700

    Convert duplicate local code into global functions

--- script.js
+++ script.js
@@ -33,170 +33,99 @@ btns.forEach((btn)=>btn.addEventListener('click', (event)=>{
 
 
     let input = btn.textContent
-
-    let current = screen.textContent
-
+    
     let ans;
 
 
 
 
-    
-    // console.log((OPERATORS.includes(input)))
+    //launch calculation
+    if(((input == '=')||(OPERATORS.includes(input))) &&
+       ((numA) && (numB) && (operator))
+    ){ 
+        //if divide by 0, prevent launch
+        if ((operator == '/')&&(numB == 0)){
+            alert(`${numA} divide by ${numB} ?
 
+            OTŌSAN!??
+            - DIVIDE / 0 not possivle
+            - enter again` )
 
+            screen.textContent = `${numA} ${operator} ...`
+        
+            return numB =  undefined; 
+        }
 
-    //EXECUTE calculation with '=' operator
-        if((input == '=') &&
-           (numA) &&
-           (operator) &&
-           (numB)
-        ){
-            
-            //DIVIDE / 0 prevention
-            if ((operator == '/') && 
-            (numB == 0)
-            ){
-                alert(`
-                    OTŌSAN!??
-                    - DIVIDE / 0 not possivle
-                    - enter again` )
+        //get answer
+        ans = startOperation(numA, operator, numB, input)
 
-                numB =  undefined;
-                
-                return screen.textContent = `enter numbre >0` 
-            } 
+        //transfer answer to numA. reset numB 
+        numA = ans;
+        numB = undefined;
 
-
-            ans = operate(numA, operator, numB)
-
-            screen.textContent = ans
-
-            numA = ans;
-            numB = undefined;
+        //reset operator 
+        if (!(input == '=')) {
+            operator = btn.textContent
+        } else {
             operator = undefined;
-        } 
-
-    //EXECUTE calculation with '!=' operator
-        if((OPERATORS.includes(input)) &&
-           (numA) &&
-           (operator) &&
-           (numB)
-        ){
-
-            //DIVIDE / 0 prevention
-            if ((operator == '/') && 
-            (numB == 0)
-            ){
-                alert(`
-                    OTŌSAN!??
-                    - DIVIDE / 0 not possivle
-                    - enter again` )
-
-                numB =  undefined;
-                
-                return screen.textContent = `enter numbre >0` 
-            } 
+        }
+    }
 
 
-            ans = operate(numA, operator, numB)
+        
+    //SET operator
+    if ((OPERATORS.includes(input) &&
+        (!operator))
+    ){
+        //console.log('OPERATOR clicked')
 
+        operator = event.target.textContent
+        //console.log(Boolean(operator))
+
+        screen.textContent = operator
+    }
+
+
+
+
+    //enable DOT POINT usage
+    if (input == '.'){
+        if(!operator){
+            numA = controlDotPoint(numA)
+        } else if (operator){
+            numB = controlDotPoint(numB)
+        }
+    }
+
+
+
+    //SET each OPERAND
+    if(NUMBERS.includes(input)){
+        if((!operator)&&(ans)){
             numA = ans;
-            operator = btn.textContent;
-            numB = undefined;
 
-            screen.textContent = ans
-        } else if ((OPERATORS.includes(input) &&
-                   (!operator))
-                ){
-            //console.log('OPERATOR clicked')
-
-            operator = event.target.textContent
-            //console.log(Boolean(operator))
-
-            screen.textContent = operator
+        } else if (!operator){
+            numA = setOperand(numA, operator, ans, input)
+             
+        } else if (operator){
+            numB = setOperand(numB, operator, ans, input)
         }
+    } 
 
 
-
-
-    //DOT POINT feature
-        if(input == '.'){
-            //console.log('. DOT CLICKED')
-
-
-            if(!operator){
-                //console.log('numA DOT clicked')
-
-                if(!numA){
-                    screen.textContent = '0.'
-                    numA = '0.'}
-
-                else if(numA.includes('.')){
-                    return alert(`you entered: '${numA}.'
-                    mandate: double dots not allowed`)
-                }
-                else{
-
-                numA += '.'
-                screen.textContent = numA
-                }
-
-            } else if(operator){
-               //console.log('numB DOT clicked')
-               
-               if(!numB){
-                screen.textContent = '0.'
-                numB = '0.'
-
-                } else if(operator&&!numB){
-                    screen.textContent = '.'
-
-                } else if(numB.includes('.')){
-                    return alert(`you entered: '${numB}.' 
-                    mandate: double dots not allowed`)
-
-                } else {
-                    numB += '.'
-                    screen.textContent = numB
-                }
-              } 
+    //enable Del function 
+    if(input === 'Del'){
+        let currentScreen = screen.textContent
+        if(numA == ans){
+            console.log("we're in")
+            return;
+        } else if (numA == currentScreen){
+            numA = undoScreen(numA, currentScreen)
         }
-    
-
-    //ASSIGN numA
-        if((NUMBERS.includes(input)) &&
-           (!operator)
-        ){
-            //console.log('numA clicked')
-
-
-            if(ans){
-                numA = ans;
-            } else if (!numA){
-                numA = input
-            } else {
-                numA += input
-            }
-            screen.textContent = numA
-
+        else if(numB == currentScreen){
+            numB = undoScreen(numB, currentScreen)
         }
-
-
-    //ASSIGN numB 
-        if((NUMBERS.includes(input)) &&
-           (operator))
-            {
-            //console.log('numB clicked')
-
-            if (!numB){
-                numB = input
-            } else {
-                numB += input
-            }
-            
-            screen.textContent = numB
-        }
+    }
 
 
     //RESET calculator
@@ -211,39 +140,6 @@ btns.forEach((btn)=>btn.addEventListener('click', (event)=>{
             screen.textContent = 0
         } 
 
-    //`Del` calculator input 
-        if(input === 'Del'){
-         //console.log('`Del` clicked')
-
-         current = screen.textContent
-
-         //DELETE numA or numB end character
-         if(current == numB){
-             if(numB.length == 1){
-                 screen.textContent = '...'
-                 numB = undefined;
-
-             } else if (numB.length > 1){
-                 numB = numB.slice(0, -1)   
-                 screen.textContent = numB
-             }
-         } else if (current == numA){
-            if(numA.length == 1){
-                screen.textContent = '...'
-                numA = undefined;
-
-            } else if (numA.length > 1){
-                numA = numA.slice(0, -1)   
-                //.slice(0, -1)  
-                // 0 specifies strings FIRST character. 
-                // -1 specifies last character.
-                // .slice() will slice, and keep everything from First Character `uptill` Last Character     
-
-                screen.textContent = numA
-            }
-        }
-    }
-
 
 
         //debug
@@ -255,6 +151,85 @@ btns.forEach((btn)=>btn.addEventListener('click', (event)=>{
 
     })
 )
+
+
+
+
+
+
+
+//FUNCTIONs
+
+// SEND DATA TO OPERATE
+function startOperation(numA, operator, numB, input){
+    // console.log('inside function')
+    // console.log(input)
+
+
+    
+    ans = operate(numA, operator, numB)
+
+    screen.textContent = ans
+
+    return ans
+}
+
+
+// PREVENT DOUBLE DOT POINTS
+function controlDotPoint(num){
+    if(!num){
+        screen.textContent = '0.'
+        return num = '0.'
+
+    } else if(num.includes('.')){
+        alert(`you entered: '${num}.'
+        mandate: double dots not allowed`)
+        return num
+
+    } else {
+        console.log('we in')
+        num += '.'
+        screen.textContent = num
+        return num
+    }
+}
+
+
+
+// SET OPERANDS
+function setOperand(num, operator, ans, input){
+    if (!num){
+        num = input
+    } else {
+        num += input
+    }
+
+    screen.textContent = num
+    
+    return num
+}
+
+
+// UNDO feature
+function undoScreen(num){
+    if(num.length == 1){
+        screen.textContent = '...'
+        num = undefined;
+        return num;
+
+    } else if (num.length > 1){
+        num = num.slice(0, -1)   
+        screen.textContent = num
+    
+        return num
+    }
+}
+
+
+
+
+
+
 
 
 
